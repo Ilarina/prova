896c4b5830ff7ec8f215e4fc0470109f48373c28
ilarina <ilarinapereira@gmail.com>
2014-03-24 14:13:51 -0300

    First Commit

---
+++ provap3/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Provap3::Application.config.secret_key_base = '17e3a942554a26ccf8db2567e433b9b15ae78997dcdcb3cb0b87cace77d6fb0ec25ecddea10da896b6ac17da637eb0f9c42f20c8247eb792bdf91d610e981555'
